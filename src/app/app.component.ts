f6e14a633e57dd0d6c2a21b9ebd68e91cc404151
kashyap antala <kashyap.antala@tatvasoft.com>
2019-12-27 15:12:24 +0530

    added ondestroy changes for polls interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatTableDataSource, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { PollsService } from './polls.service';
 import { JsonDiologueComponent } from './json-diologue/json-diologue.component';
@@ -15,7 +15,7 @@ export interface PeriodicElement {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ai-test-dhaval';
   displayedColumns: string[] = ['title', 'url', 'created_at', 'author'];
   dataSource: any;
@@ -26,6 +26,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.getData();
     this.setPollsInterval();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
   }
 
   getData() {
